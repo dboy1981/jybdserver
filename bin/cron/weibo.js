58b25369032445606044f03c9c17ad1f2ca60790
dboy <zhaoxin@txwy.com>
2021-07-15 09:55:13 +0800

    修改抓取时间

--- bin/cron/weibo.js
+++ bin/cron/weibo.js
@@ -1,7 +1,7 @@
 var CronJob = require('cron').CronJob;
 const { crawl } = require('../../controlers/weibo')
 var job = new CronJob(
-	'00 05 00 * * *',
+	'00 30 00 * * *',
 	function() {
 		const d = new Date();
 		console.log(d)
